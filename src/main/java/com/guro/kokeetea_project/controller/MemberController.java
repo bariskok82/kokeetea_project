8c0f9408f913a76308b8effc6d73bbcc3264c8f4
bariskok82 <geunmok@hanmail.net>
2022-11-23 12:02:39 +0900

    final edit

--- src/main/java/com/guro/kokeetea_project/controller/MemberController.java
+++ src/main/java/com/guro/kokeetea_project/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.guro.kokeetea_project.controller;
 
-import com.guro.kokeetea_project.dto.MemberFormDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-    @GetMapping(value = "/new")
-    public String newMember(Model model){
-        model.addAttribute("memberFormDto", new MemberFormDto());
-        return "member/register";
-    }
-
     @GetMapping(value = "/login")
     public String loginMember(){
         return "member/login";
